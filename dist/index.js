c04f3a350436c7472839bbbe2ea15ac148634b8b
fedeghe <fedeghe@gmail.com>
2021-01-19 22:37:53 +0100

    basic setup

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+'use strict';
+/*
+
+justaniframe
+v. 0.0.0
+
+*/
+var justaniframe=void 0;"object"==typeof exports&&(module.exports=justaniframe);
---
+++ source/index.js
@@ -0,0 +1,5 @@
+var justaniframe = (function () {
+    
+})();
+
+(typeof exports === 'object') && (module.exports = justaniframe);
